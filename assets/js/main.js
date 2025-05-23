8198321c5622ea930fa5badd15697e4d8fe2e0aa
JisunKim32 <jenny.kim22856@gmail.com>
2025-05-23 16:23:27 +0900

    찐최종

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,97 +28,97 @@ let q = {
 let result = {
     "ISTJ" : {"resultTitle":"<span style=color:#708090>ISTJ</span>", 
 
-        "explain":"<span style=\"color:#FF7BA0\">“ 슬레이트 그레이의 정석연애 ”</span> <br>연애에도 매뉴얼이 있어야 안심되는 스타일<br>다 계획했는데 상대가 즉흥적이면 당황해버림. <br><br><br><br>#직진보단계획 #연애도절차대로 #현실중심",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px;font-size:30px\">“ 슬레이트 그레이의 정석연애 ”</span> <br>연애에도 매뉴얼이 있어야 안심되는 스타일<br>다 계획했는데 상대가 즉흥적이면 당황해버림. <br><br><br><br>#직진보단계획 #연애도절차대로 #현실중심",
 
         "img": "assets/images/ISTJ.png"},
 
     "ISFJ" : {"resultTitle":"<span style=color:#EFDCC1>ISFJ</span>",
 
-         "explain":"<span style=\"color:#FF7BA0\">“ 크림 베이지의 배려형 ”</span><br>상대를 먼저 생각하고 배려하지만, <br>나 자신은 자주 놓쳐버려요.너무 참다 서운해짐.<br><br><br><br>#배려왕 #속앓이전문 #착해서문제야",
+         "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 크림 베이지의 배려형 ”</span><br>상대를 먼저 생각하고 배려하지만, <br>나 자신은 자주 놓쳐버려요.너무 참다 서운해짐.<br><br><br><br>#배려왕 #속앓이전문 #착해서문제야",
          
          "img": "assets/images/ISFJ.png"},
 
     "INFJ" : {"resultTitle":"<span style=color:#B9AEDC>INFJ</span>",
         
-        "explain":"<span style=\"color:#FF7BA0\">“ 라벤더빛 내면형 연애 ”</span><br>진심으로 사랑하지만 표현이 어려워.<br>머릿속 시나리오와 현실은 늘 어긋나 있어요.<br><br><br><br>#감정시뮬레이터 #연애철학자 #속깊은사람",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 라벤더빛 내면형 연애 ”</span><br>진심으로 사랑하지만 표현이 어려워.<br>머릿속 시나리오와 현실은 늘 어긋나 있어요.<br><br><br><br>#감정시뮬레이터 #연애철학자 #속깊은사람",
         
         "img": "assets/images/INFJ.png"},
 
     "ISTP" : {"resultTitle":"<span style=color:#4682B4>ISTP</span>",
 
-         "explain":"<span style=\"color:#FF7BA0\">“ 스틸 블루의 무심한 감정 ”</span><br>관계에 크게 집착하지 않지만,<br> 상대는 그게 무심함으로 느껴질 수 있어요.<br><br><br><br>#무심한관심 #쿨한척진지 #혼자가편해",
+         "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 스틸 블루의 무심한 감정 ”</span><br>관계에 크게 집착하지 않지만,<br> 상대는 그게 무심함으로 느껴질 수 있어요.<br><br><br><br>#무심한관심 #쿨한척진지 #혼자가편해",
          
          "img": "assets/images/ISTP.png"},
 
     "INTJ" : {"resultTitle":"<span style=color:#2F2F2F>INTJ</span>", 
 
-        "explain":"<span style=\"color:#FF7BA0\">“ 차콜 블랙의 전략적 연애 ”</span><br>연애도 전략적으로 접근하지만,<br>감정의 흐름에는 약해요. <br>감정 표현,진심 어려움.<br><br><br><br>#연애로직충 #쿨한척 #실은쑥스러움",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 차콜 블랙의 전략적 연애 ”</span><br>연애도 전략적으로 접근하지만,<br>감정의 흐름에는 약해요. <br>감정 표현,진심 어려움.<br><br><br><br>#연애로직충 #쿨한척 #실은쑥스러움",
         
         "img": "assets/images/INTJ.png"},
 
     "ISFP" : {"resultTitle":"<span style=color:#E7BFB1>ISFP</span>", 
 
-        "explain":"<span style=\"color:#FF7BA0\">“ 더스티 핑크의 조용한 감성 ”</span><br>좋아하는 감정은 진짜 깊지만<br>표현하는 건 어려워요.<br>분위기와 감성에 약한 순정파.<br><br><br><br>#순정러 #눈치백단 #다가가줘",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 더스티 핑크의 조용한 감성 ”</span><br>좋아하는 감정은 진짜 깊지만<br>표현하는 건 어려워요.<br>분위기와 감성에 약한 순정파.<br><br><br><br>#순정러 #눈치백단 #다가가줘",
         
         "img": "assets/images/ISFP.png"},
 
     "INFP" : {"resultTitle":"<span style=color:#F4AAB9>INFP</span>", 
 
-        "explain":"<span style=\"color:#FF7BA0\">“ 블러시 로즈의 상상 연애 ”</span><br>머릿속에선 이미 연애 중인데 <br>현실에선 말 한마디 못 꺼냄.<br>감정선이 너무 깊어요.<br><br><br><br>#혼자연애중 #상상은자유 #말못하는로맨티스트",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 블러시 로즈의 상상 연애 ”</span><br>머릿속에선 이미 연애 중인데 <br>현실에선 말 한마디 못 꺼냄.<br>감정선이 너무 깊어요.<br><br><br><br>#혼자연애중 #상상은자유 #말못하는로맨티스트",
         
         "img": "assets/images/INFP.png"},
 
     "INTP" : {"resultTitle":"<span style=color:#6C7DAB>INTP</span>", 
         
-        "explain":"<span style=\"color:#FF7BA0\">“ 아이스 인디고의 논리적 연애 ”</span><br>감정보다는 논리가 먼저 움직이는 타입.<br>타이밍 놓치기 일쑤예요.<br><br><br><br>#머리로연애함 #감정처리딜레이 #표현약함",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 아이스 인디고의 논리적 연애 ”</span><br>감정보다는 논리가 먼저 움직이는 타입.<br>타이밍 놓치기 일쑤예요.<br><br><br><br>#머리로연애함 #감정처리딜레이 #표현약함",
         
         "img": "assets/images/INTP.png"},
 
     "ESTJ" : {"resultTitle":"<span style=color:#8B5E3C>ESTJ</span>", 
         
-        "explain":"<span style=\"color:#FF7BA0\">“ 어스 브라운의 목표 지향형 ”</span><br>연애도 목표와 책임이 중요하다고 생각해요. <br>명확한 관계, 불확실한 건 싫어요!<br><br><br><br>#연애리더십 #직설적인사랑 #계획형연애",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 어스 브라운의 목표 지향형 ”</span><br>연애도 목표와 책임이 중요하다고 생각해요. <br>명확한 관계, 불확실한 건 싫어요!<br><br><br><br>#연애리더십 #직설적인사랑 #계획형연애",
         
         "img": "assets/images/ESTJ.png"},
 
     "ESFJ" : {"resultTitle":"<span style=color:#FFB9CF>ESFJ</span>",
         
-        "explain":"<span style=\"color:#FF7BA0\">“ 소프트 핑크의 세심한 배려 ”</span><br>표현도 잘하고, 상대 기분도 잘 챙겨줘요. <br>하지만 너무 헌신적이면 지치기도 해요.<br><br><br><br>#애정폭발 #케어마스터 #다주는연애",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 소프트 핑크의 세심한 배려 ”</span><br>표현도 잘하고, 상대 기분도 잘 챙겨줘요. <br>하지만 너무 헌신적이면 지치기도 해요.<br><br><br><br>#애정폭발 #케어마스터 #다주는연애",
         
         "img": "assets/images/ESFJ.png"},
 
     "ENFJ" : {"resultTitle":"<span style=color:#D65A79>ENFJ</span>",
         
-        "explain":"<span style=\"color:#FF7BA0\">“ 로즈 레드의 헌신 연애 ”</span><br>배려심 깊고 헌신적인 연애를 해요. <br>하지만 상대가 그만큼 안 돌아오면<br>상처받기 쉬워요.<br><br><br><br>#연애리더 #희생형사랑 #기대금지",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 로즈 레드의 헌신 연애 ”</span><br>배려심 깊고 헌신적인 연애를 해요. <br>하지만 상대가 그만큼 안 돌아오면<br>상처받기 쉬워요.<br><br><br><br>#연애리더 #희생형사랑 #기대금지",
         
         "img": "assets/images/ENFJ.png"},
 
     "ENTJ" : {"resultTitle":"<span style=color:#223A5E>ENTJ</span>",
         
-        "explain":"<span style=\"color:#FF7BA0\">“ 네이비 블루의 주도적 스타일 ”</span><br>열정 있고 추진력도 넘치지만, <br>감정 조절엔 미숙할 때가 있어요.<br>연애도 성과처럼 생각할 때가 있죠.<br><br><br><br>#사랑도목표 #연애에계획서 #표현은어색",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 네이비 블루의 주도적 스타일 ”</span><br>열정 있고 추진력도 넘치지만, <br>감정 조절엔 미숙할 때가 있어요.<br>연애도 성과처럼 생각할 때가 있죠.<br><br><br><br>#사랑도목표 #연애에계획서 #표현은어색",
         
         "img": "assets/images/ENTJ.png"},
 
     "ESTP" : {"resultTitle":"<span style=color:#FF4D4D>ESTP</span>",
         
-        "explain":"<span style=\"color:#FF7BA0\">“ 비비드 레드의 즉흥 연애 ”</span><br>순간의 감정에 충실하고 즉흥적인 연애를 즐겨요. <br>다만 깊은 관계는 잘 안 이어질 수도.<br><br><br><br>#불도저연애 #직진스타일 #깊이보다속도",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 비비드 레드의 즉흥 연애 ”</span><br>순간의 감정에 충실하고 즉흥적인 연애를 즐겨요. <br>다만 깊은 관계는 잘 안 이어질 수도.<br><br><br><br>#불도저연애 #직진스타일 #깊이보다속도",
         
         "img": "assets/images/ESTP.png"},
 
     "ESFP" : {"resultTitle":"<span style=color:#FFAD94>ESFP</span>",
         
-        "explain":"<span style=\"color:#FF7BA0\">“ 코랄 피치의 활기찬 감성 ”</span><br>감정표현이 능숙하고 매력도 넘치지만,<br>감정 기복도 커요. 사랑도 자유롭게 즐김.<br><br><br><br>#핫한감성 #감정롤러코스터 #사랑꾼",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 코랄 피치의 활기찬 감성 ”</span><br>감정표현이 능숙하고 매력도 넘치지만,<br>감정 기복도 커요. 사랑도 자유롭게 즐김.<br><br><br><br>#핫한감성 #감정롤러코스터 #사랑꾼",
         
         "img": "assets/images/ESFP.png"},
 
     "ENFP" : {"resultTitle":"<span style=color:#FF8855>ENFP</span>", 
         
-        "explain":"<span style=\"color:#FF7BA0\">“ 선셋 오렌지의 열정 연애 ”</span><br>새로운 사람과의 설렘을 즐기고 표현도 잘해요. <br>근데 감정이 너무 왔다갔다 할 때도.<br><br><br><br>#썸마스터 #열정과자유 #표현과다",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 선셋 오렌지의 열정 연애 ”</span><br>새로운 사람과의 설렘을 즐기고 표현도 잘해요. <br>근데 감정이 너무 왔다갔다 할 때도.<br><br><br><br>#썸마스터 #열정과자유 #표현과다",
         
         "img": "assets/images/ENFP.png"},
 
     "ENTP" : {"resultTitle":"<span style=color:#>ENTP</span>",
         
-        "explain":"<span style=\"color:#FF7BA0\">“ 레몬 옐로우의 유쾌한 대화형 ”</span><br>대화 잘하고 매력적인데, <br>진심을 헷갈리게 만드는 <br>장난스러운 스타일.<br><br><br><br>#말빨장인 #웃다가혼나기 #애매한경계",
+        "explain":"<span style=\"color:#FF7BA0;font-size:30px\">“ 레몬 옐로우의 유쾌한 대화형 ”</span><br>대화 잘하고 매력적인데, <br>진심을 헷갈리게 만드는 <br>장난스러운 스타일.<br><br><br><br>#말빨장인 #웃다가혼나기 #애매한경계",
         
         "img": "assets/images/ENTP.png"},
 
